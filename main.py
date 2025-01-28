68fdb6dd398c90b29f93ab3c42f760b8b8d9b2b8
Stephen-Hrrll <stephenhrrll8@gmail.com>
2025-01-28 08:56:44 -0500

    implement file read

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def main():
-    print("hello world")
+
+    path_pref = "/home/zero/workspace/github.com/Stephen-Hrrll/bookbot/books/"
+    title = "frankenstein"
+    extension = ".txt"
+
+    path_to_file = path_pref + title + extension
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    print(file_contents)
 
 
 
