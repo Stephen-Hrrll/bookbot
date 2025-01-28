0cd612231c58fc9ed6e66e1cf453d5f4f658c4b1
Stephen-Hrrll <stephenhrrll8@gmail.com>
2025-01-28 09:34:49 -0500

    implement count characters

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+def count_characters(text_str: str):
+    """Takes a string of text from a book converts all characters to 
+    lowercase and returns a dict with characters as the key and the 
+    number of times that character appeared in the string. 
+    (String) ==> {Char:Count}
+    """
+    char_count = {}
+
+    for ch in text_str:
+        ch_lower = ch.lower()
+        if ch_lower not in char_count:
+            char_count[ch_lower] = 1
+        else:
+            char_count[ch_lower] += 1
+
+    return char_count
+
 def count_words(text_str: str):
     str_list = text_str.split()
     count = 0
@@ -19,8 +36,9 @@ def main():
     with open(path_to_file) as f:
         file_contents = f.read()
 
-    print(file_contents)
-    print(count_words(file_contents))
+    # print(file_contents)
+    # print(count_words(file_contents))
+    print(count_characters(file_contents))
 
 
 
